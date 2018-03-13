8e7d75ab8c85533b7c8610c6ee0b0137a07b81d2
Felice Serena <felice@serena-mueller.ch>
2018-03-13 21:20:14 +0100

    Draft cluster descriptor

---
+++ lib/generic/cluster_descriptor.h
@@ -0,0 +1,26 @@
+/// \file
+/// Maintainer: Felice Serena
+///
+///
+
+#pragma once
+
+#include<vector>
+
+namespace Mousetrack {
+
+
+/// This is an abstract class. Derive from this class to create a usable cluster descriptor.
+class ClusterDescriptor {
+public:
+	/// Returns a distance/norm `n` between `this` and `other`.
+	/// It should be small for two instances that are "similar" to each other, 
+	/// and large for two instances that are very different.
+	/// Important properties:
+	/// 1. 0 <= n <= inf
+	/// 2. if this == other -> n == 0
+	double compare(const ClusterDescriptor* other) const;
+};
+
+
+}
