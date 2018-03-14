30fd495a2f1b93661967d890afd65c8a5174b431
Felice Serena <felice@serena-mueller.ch>
2018-03-14 20:00:17 +0100

    Adjusted Trajectory

--- lib/generic/trajectory.h
+++ lib/generic/trajectory.h
@@ -14,9 +14,7 @@ namespace Mousetrack {
 /// Describes a path in 3D space
 class Trajectory {
 public:
-	/// N x 3 matrix, each row holds the (x,y,z) coordinates of a 3D point.
-	Eigen::MatrixXd& controlPoints();
-	const Eigen::MatrixXd& controlPoints() const;
+	std::map<size_t, std::shared_ptr<const ClusterDescriptor>> controlPoints;
 };
 
 
