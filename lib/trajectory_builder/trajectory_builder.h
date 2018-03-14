6a49c6f553011a48d2440908c0e53f47729a4887
Felice Serena <felice@serena-mueller.ch>
2018-03-14 17:49:07 +0100

    Adjusted TrajectoryBuilder interface to better support streaming

--- lib/trajectory_builder/trajectory_builder.h
+++ lib/trajectory_builder/trajectory_builder.h
@@ -9,11 +9,18 @@
 
 namespace Mousetrack {
 
+struct TrajectoryFrame {
+	/// Think of it as the time `t` taking integer values
+	size_t frameNumber;
+	std::vector<std::shared_ptr<const ClusterDescriptor> clusters;
+}
 
 class TrajectoryBuilder {
 public:
 	/// Aggregates descriptors from different frames into trajectories.
-	virtual std::vector<Trajectory> operator(const std::vector<std::vector<std::unique_ptr<ClusterDescriptor>>>& descriptors) = 0;
+	/// descriptors: a list of frames, holding the frame number and a list of descriptors
+	/// trajectories: Descriptors are added to existing trajectories, if similar enough, new trajectories might be created
+virtual void operator(const std::vector<TrajectoryFrame>& descriptors, std::vector<Trajectory>& trajectories) = 0;
 };
 
 
