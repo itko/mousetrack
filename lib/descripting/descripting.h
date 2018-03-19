42bc53ddd9b6619f53e24d4d54899ea0e6f2ed0a
Felice Serena <felice@serena-mueller.ch>
2018-03-20 00:29:38 +0100

    Integrated descripting

--- lib/descripting/descripting.h
+++ lib/descripting/descripting.h
@@ -5,16 +5,19 @@
 
 #pragma once
 
-#include<vector>
+#include "generic/cluster.h"
+#include "generic/cluster_descriptor.h"
+#include "generic/point_cloud.h"
+#include <memory>
+#include <vector>
 
-namespace Mousetrack {
-
+namespace MouseTrack {
 
 class Descripting {
 	public:
 		/// Takes a cluster and its referencing PointCloud and creates a cluster descriptor
-		virtual std::unique_ptr<ClusterDescriptor> operator(const Cluster& clusters, const PointCloud& cloud) const = 0;
+        virtual std::unique_ptr<ClusterDescriptor> operator()(const Cluster& cluster, const PointCloud& cloud) const = 0;
 };
 
 
-}
+} // MouseTrack
