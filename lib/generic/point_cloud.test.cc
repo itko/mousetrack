6e94d01ab00b2697c1619ac4a1823c746c7cb884
Felice Serena <felice@serena-mueller.ch>
2018-03-20 14:43:34 +0100

    Corrections from self-review

--- lib/generic/point_cloud.test.cc
+++ lib/generic/point_cloud.test.cc
@@ -17,12 +17,14 @@ BOOST_AUTO_TEST_CASE( point_cloud_read_write ) {
     pc[0].x() = 1.0;
     pc[0].y() = 2.0;
     pc[0].z() = 3.0;
+    pc[0].intensity() = 4.0;
 
     const auto point = pc[0];
 
     BOOST_CHECK_CLOSE( 1.0, point.x(), .00001);
     BOOST_CHECK_CLOSE( 2.0, point.y(), .00001);
     BOOST_CHECK_CLOSE( 3.0, point.z(), .00001);
+    BOOST_CHECK_CLOSE( 4.0, point.intensity(), .00001);
 }
 
 
