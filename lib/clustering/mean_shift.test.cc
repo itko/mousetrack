70cb46886ae5649e68cd36f00b45451a327cf9c7
Luzian Hug <luhug@student.ethz.ch>
2018-04-03 19:02:00 +0200

    Added gaussian cluster test - test is positive!

--- lib/clustering/mean_shift.test.cc
+++ lib/clustering/mean_shift.test.cc
@@ -8,6 +8,7 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 #include <stdlib.h>
+#include <random>
 
 
 namespace utf = boost::unit_test;
@@ -65,4 +66,42 @@ BOOST_AUTO_TEST_CASE( two_closetogether_points ) {
 
   std::vector<MouseTrack::Cluster> clusters = ms(pc);
   BOOST_CHECK_EQUAL(clusters.size(),1);
+}
+
+BOOST_AUTO_TEST_CASE( three_gaussian_clusters ) {
+  std::default_random_engine gen;
+
+  std::normal_distribution<double> gauss0(0.0,1.0);
+  std::normal_distribution<double> gauss10(10.0,1.0);
+
+  MouseTrack::PointCloud pc;
+  pc.resize(300);
+
+  MouseTrack::MeanShift ms = MouseTrack::MeanShift(1);
+
+  for (int i = 0; i<300; i+=3) {
+    pc[i].x() = gauss10(gen);
+    pc[i].y() = gauss0(gen);
+    pc[i].z() = gauss0(gen);
+    pc[i].intensity() = gauss0(gen);
+
+    pc[i+1].x() = gauss0(gen);
+    pc[i+1].y() = gauss10(gen);
+    pc[i+1].z() = gauss0(gen);
+    pc[i+1].intensity() = gauss0(gen);
+
+    pc[i+2].x() = gauss0(gen);
+    pc[i+2].y() = gauss0(gen);
+    pc[i+2].z() = gauss10(gen);
+    pc[i+2].intensity() = gauss0(gen);
+  }
+
+  std::vector<MouseTrack::Cluster> clusters = ms(pc);
+
+  BOOST_CHECK_EQUAL(clusters.size(),3);
+  BOOST_CHECK_EQUAL(clusters[0].points().size(),100);
+  BOOST_CHECK_EQUAL(clusters[1].points().size(),100);
+  BOOST_CHECK_EQUAL(clusters[2].points().size(),100);
+
+
 }
