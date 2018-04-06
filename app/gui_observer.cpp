218ea805cbc437dba0562dcc4fff2d40ce0daccf
Luzian Hug <luhug@student.ethz.ch>
2018-04-06 14:46:46 +0200

    added GUIObserver

---
+++ app/gui_observer.cpp
@@ -0,0 +1,74 @@
+/// \file
+/// Maintainer: Luzian Hug
+///
+
+
+#pragma once
+
+#include "pipeline_observer.h"
+
+
+namespace MouseTrack {
+
+
+
+    GUIObserver::GUIObserver(QWidget* widget) : _widget(widget) {
+      //empty
+    }
+
+    void GUIObserver::pipelineStarted() {
+      //empty
+    }
+
+    void GUIObserver::pipelineTerminated() {
+      //empty
+    }
+
+    void GUIObserver::frameStart(FrameIndex frame) {
+      //empty
+    }
+
+    void GUIObserver::frameEnd(FrameIndex frame) {
+      _widget->update();
+    }
+
+    void GUIObserver::startFrameWindow     (FrameIndex f) {
+      //empty
+    }
+    void GUIObserver::newFrameWindow       (FrameIndex f, std::shared_ptr<const FrameWindow> window) {
+      _widget->setFrameWindow(window);
+    }
+    void GUIObserver::startRegistration    (FrameIndex f) {
+      //empty
+    }
+    void GUIObserver::newRawPointCloud     (FrameIndex f, std::shared_ptr<const PointCloud> cloud) {
+      _widget->setPointCloud(cloud);
+    }
+    void GUIObserver::startClustering      (FrameIndex f) {
+      //empty
+    }
+    void GUIObserver::newClusters          (FrameIndex f, std::shared_ptr<const std::vector<Cluster>> clusters) {
+      _widget->setClusters(clusters);
+    }
+    void GUIObserver::startDescripting     (FrameIndex f) {
+      //empty
+    }
+    void GUIObserver::newDescriptors       (FrameIndex f, std::shared_ptr<const std::vector<std::shared_ptr<const ClusterDescriptor>>> descriptors) {
+      _widget->setDescriptors(descriptors);
+    }
+    void GUIObserver::startMatching        (FrameIndex f) {
+      //empty
+    }
+    void GUIObserver::newMatches           (FrameIndex f, std::shared_ptr<const std::vector<long>> matches) {
+      _widget->setMatches(matches);
+    }
+    void GUIObserver::startControlPoints   (FrameIndex f) {
+      //empty
+    }
+    void GUIObserver::newControlPoints     (FrameIndex f, std::shared_ptr<const std::vector<Eigen::Vector3d>> controlPoints) {
+      _widget->setControlPoints(controlPoints);
+    }
+
+};
+
+} // MouseTrack
---
+++ app/gui_observer.h
@@ -0,0 +1,41 @@
+/// \file
+/// Maintainer: Luzian Hug
+///
+
+
+#pragma once
+
+#include "pipeline_observer.h"
+
+
+namespace MouseTrack {
+
+class GUIObserver: public PipelineObserver {
+public:
+    GUIObserver(QWidget* widget);
+
+    void pipelineStarted();
+
+    void pipelineTerminated();
+
+    void frameStart(FrameIndex frame);
+
+    void frameEnd(FrameIndex frame);
+
+    void startFrameWindow     (FrameIndex f);
+    void newFrameWindow       (FrameIndex f, std::shared_ptr<const FrameWindow> window);
+    void startRegistration    (FrameIndex f);
+    void newRawPointCloud     (FrameIndex f, std::shared_ptr<const PointCloud> cloud);
+    void startClustering      (FrameIndex f);
+    void newClusters          (FrameIndex f, std::shared_ptr<const std::vector<Cluster>> clusters);
+    void startDescripting     (FrameIndex f);
+    void newDescriptors       (FrameIndex f, std::shared_ptr<const std::vector<std::shared_ptr<const ClusterDescriptor>>> descriptors);
+    void startMatching        (FrameIndex f);
+    void newMatches           (FrameIndex f, std::shared_ptr<const std::vector<long>> matches);
+    void startControlPoints   (FrameIndex f);
+    void newControlPoints     (FrameIndex f, std::shared_ptr<const std::vector<Eigen::Vector3d>> controlPoints);
+private:
+    CameraViewWidget* _widget;
+};
+
+} // MouseTrack
