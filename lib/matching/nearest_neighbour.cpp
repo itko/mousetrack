0592736fbff8dcc901f57315b2f5adca3b5cf57b
Itto Kornecki <ittokor@gmail.com>
2018-04-09 11:55:54 +0200

    Prevent splitting/merging or cluster chains when matching

--- lib/matching/nearest_neighbour.cpp
+++ lib/matching/nearest_neighbour.cpp
@@ -18,29 +18,31 @@ std::vector<long> NearestNeighbour::operator()(
     const std::vector<ClusterChain> &chains) {
   // Prepare an empty vector that will store the index of each chain
   std::vector<long> matchIndex;
-  for (size_t i = 0; i < chains.size(); i++) {
-    // Initialize a maximum distance
+  for (size_t i = 0; i < descriptors.size(); i++) {
+    // Initialize a maximum distance and match index
     double nearest = std::numeric_limits<double>::max();
-    // In this case we are only really concerned with the last cluster.
-    // Get the frame number and the descriptor
-    auto descriptor = chains[i].descriptors().rbegin();
-    // Get the descriptor for that frame.
-    auto lastDescriptor = descriptor->second;
-    for (size_t j = 0; j < descriptors.size(); j++) {
-      // Compare the descriptor to the cluster. If it's closer than the previous
-      // chain then choose that one.
-      // TODO This means that there may be multipel chains pointing to the
-      // same descriptor.
-      long match = 0;
-      double distance = descriptors[j]->compare(lastDescriptor.get());
-      if (nearest > distance) {
-        // Better match than previous chain. Pick that one.
-        match = i;
-        nearest = distance;
+    long match = -1;
+    for (size_t j = 0; j < chains.size(); j++) {
+      // Check if we already assigned this chain
+      if (std::find(matchIndex.begin(), matchIndex.end(), j) ==
+          matchIndex.end()) {
+        // We are only really concerned with the last cluster.
+        // Get the frame number and the descriptor
+        auto descriptor = chains[j].descriptors().rbegin();
+        // Get the descriptor for that frame.
+        auto lastDescriptor = descriptor->second;
+        // Compare the descriptor to the cluster. If it's closer than the
+        // previous chain then choose that one.
+        double distance = descriptors[i]->compare(lastDescriptor.get());
+        if (nearest > distance) {
+          // Better match than previous chain. Pick that one.
+          match = j;
+          nearest = distance;
+        }
       }
-      // Done comparing. Push.
-      matchIndex.push_back(match);
     }
+    // Done comparing. Push.
+    matchIndex.push_back(match);
   }
   return matchIndex;
 };
