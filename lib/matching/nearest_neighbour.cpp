f2b6d789a5824488422571421569fd5f89b5a50e
Felice Serena <felice@serena-mueller.ch>
2018-04-14 13:02:33 +0200

    Reduced output of NNMatching

--- lib/matching/nearest_neighbour.cpp
+++ lib/matching/nearest_neighbour.cpp
@@ -20,7 +20,9 @@ std::vector<long> NearestNeighbour::operator()(
   // Prepare an empty vector that will store the index of each chain
   std::vector<long> matchIndex;
   for (size_t i = 0; i < descriptors.size(); i++) {
-    BOOST_LOG_TRIVIAL(trace) << "Matching descriptor " << i;
+    if(i%100 == 0){
+        BOOST_LOG_TRIVIAL(trace) << "Matching descriptor " << i;
+    }
     // Initialize a maximum distance and match index
     double nearest = std::numeric_limits<double>::max();
     long match = -1;
