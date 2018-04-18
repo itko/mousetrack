375e3fe565cd4b3d56504fd7422939dc0faeb40e
Felice Serena <felice@serena-mueller.ch>
2018-04-18 23:52:29 +0200

    try-catch

--- app/main.cpp
+++ app/main.cpp
@@ -44,63 +44,73 @@ void setLogLevel(const std::string &level) {
 }
 
 int main(int argc, char *argv[]) {
-  op::options_description option_desc = cli_options();
+  try {
+    op::options_description option_desc = cli_options();
 
-  op::positional_options_description pos_desc;
-  // allow to shorten "-src-dir <path>" to "<path>"
-  pos_desc.add("src-dir", -1);
-  op::command_line_parser parser{argc, argv};
-  parser.options(option_desc).positional(pos_desc).allow_unregistered();
-  op::parsed_options parsed_options = parser.run();
-  op::variables_map cli_options;
-  op::store(parsed_options, cli_options);
+    op::positional_options_description pos_desc;
+    // allow to shorten "-src-dir <path>" to "<path>"
+    pos_desc.add("src-dir", -1);
+    op::command_line_parser parser{argc, argv};
+    parser.options(option_desc).positional(pos_desc).allow_unregistered();
+    op::parsed_options parsed_options = parser.run();
+    op::variables_map cli_options;
+    op::store(parsed_options, cli_options);
 
-  if (cli_options.count("help")) {
-    std::cout << option_desc;
-    return 0;
-  }
-
-  setLogLevel(cli_options["log"].as<std::string>());
-
-  const bool gui_mode = cli_options.count("cli") == 0;
-
-  std::unique_ptr<Controller> controller;
-
-  if (gui_mode) {
-#ifdef ENABLE_GUI
-    controller = std::unique_ptr<QtController>(new QtController());
-#else
-    std::cerr << "Binary was not compiled with a GUI option. See --help for "
-                 "other options.";
-    return -1;
-#endif
-  } else {
-    if (cli_options.count("src-dir") == 0) {
-      std::cout << "Source path must not be empty for command line interface. "
-                   "See --help for further options."
-                << std::endl;
-      return -1;
+    if (cli_options.count("help")) {
+      std::cout << option_desc;
+      return 0;
     }
-    controller = std::unique_ptr<CliController>(new CliController());
-    // controller->setPipeline(pipelineFactory.fromCliOptions(cli_options));
-  }
-  PipelineFactory pipelineFactory;
-  controller->pipeline() =
-      std::move(pipelineFactory.fromCliOptions(cli_options));
 
-  PipelineTimer timer;
-  if (cli_options.count("pipeline-timer")) {
-    controller->pipeline().addObserver(&timer);
-  }
+    setLogLevel(cli_options["log"].as<std::string>());
 
-  std::unique_ptr<PipelineWriter> writer;
-  if (cli_options.count("out-dir")) {
-    writer = std::make_unique<PipelineWriter>(
-        cli_options["out-dir"].as<std::string>());
-    controller->pipeline().addObserver(writer.get());
-  }
+    const bool gui_mode = cli_options.count("cli") == 0;
 
-  int errorCode = controller->main(argc, argv, cli_options);
-  std::cout << std::flush;
-  return errorCode;
+    std::unique_ptr<Controller> controller;
+
+    if (gui_mode) {
+#ifdef ENABLE_GUI
+      controller = std::unique_ptr<QtController>(new QtController());
+#else
+      std::cerr << "Binary was not compiled with a GUI option. See --help for "
+                   "other options.";
+      return -1;
+#endif
+    } else {
+      if (cli_options.count("src-dir") == 0) {
+        std::cout
+            << "Source path must not be empty for command line interface. "
+               "See --help for further options."
+            << std::endl;
+        return -1;
+      }
+      controller = std::unique_ptr<CliController>(new CliController());
+      // controller->setPipeline(pipelineFactory.fromCliOptions(cli_options));
+    }
+    PipelineFactory pipelineFactory;
+    controller->pipeline() =
+        std::move(pipelineFactory.fromCliOptions(cli_options));
+
+    PipelineTimer timer;
+    if (cli_options.count("pipeline-timer")) {
+      controller->pipeline().addObserver(&timer);
+    }
+
+    std::unique_ptr<PipelineWriter> writer;
+    if (cli_options.count("out-dir")) {
+      writer = std::make_unique<PipelineWriter>(
+          cli_options["out-dir"].as<std::string>());
+      controller->pipeline().addObserver(writer.get());
+    }
+
+    int errorCode = controller->main(argc, argv, cli_options);
+    std::cout << std::flush;
+    return errorCode;
+  } catch (const std::string s) {
+    std::cerr << "Exception: " << s << std::endl;
+  } catch (const char *s) {
+    std::cerr << "Exception: " << s << std::endl;
+  } catch (const int i) {
+    std::cerr << "Exception: " << i << std::endl;
+  }
+  return -1;
 }
