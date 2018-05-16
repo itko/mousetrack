74c4f1b1f4703acfcda77794d197642c2ab70710
Luzian Hug <Luzian@Luzian-Hug.local>
2018-05-16 18:27:00 +0200

    bla

--- scripts/validation.py
+++ scripts/validation.py
@@ -23,53 +23,56 @@ def read_matrix_chain(csv_path):
         csv_reader = csv.reader(csv_file)
         matrices = []
         for matrix in csv_reader:
-            matrices.append(np.reshape(matrix,CAMCHAIN_MAT_DIM).T) # Transpose because MATLAB reshape is column wise and numpy is row-wise
-            return matrices
+            matrices.append(np.asarray(np.reshape(matrix,CAMCHAIN_MAT_DIM).T,dtype='float64'))# Transpose because MATLAB reshape is column wise and numpy is row-wise
+        return matrices
 
 def read_params(csv_path):
     csv_path = os.path.abspath(csv_path)
-    params = list(N_STREAMS)
+    params = []
     with open(csv_path) as csv_file:
         csv_reader = csv.reader(csv_file)
-        for i,entry in csv_reader:
-            params[i] = map(int,entry)
-            
+        for entry in csv_reader:
+            params.append(entry)
+        return params
+
 def read_controlpoint(csv_path):
     csv_path = os.path.abspath(csv_path)
     with open(csv_path) as csv_file:
         csv_reader = csv.reader(csv_file)
-        point = np.asarray([next(csv_reader)])
+        point = np.asarray([next(csv_reader)],dtype='float64')
         return point.T
-            
+
 def projection_matrix(camchain,R,cam):
     matrix = R[cam]
     for i in range(2*cam + 1):
-        matrix = matrix * camchain[i]
-    return matrix
-    
+        matrix = np.matmul(matrix , camchain[i])
+    return np.asarray(matrix,dtype='float64')
+
 def project_point(control_point, projection_matrix, params):
-    focallength = params[0]
-    ccx = params[2]
-    ccy = params[3]
-    cp_hom = np.column_stack((control_point,1))
-    p4D = projection_matrix * cp_hom
+    focallength = float(params[0])
+    ccx = float(params[2])
+    ccy = float(params[3])
+    cp_hom = np.row_stack((control_point,[[1]]))
+    p4D = np.matmul(projection_matrix , cp_hom)
+
     p2D = np.array([p4D[0]/p4D[3]/focallength, p4D[1]/p4D[3]/focallength])
-    cc = np.array([ccx, ccy])
-    shift = np.array([X_SHIFT,Y_SHIFT])
+    cc = np.array([[ccx, ccy]]).T
+    shift = np.array([[X_SHIFT,Y_SHIFT]]).T
+    print(shift.shape)
     return p2D - shift + cc
 
 if __name__ == "__main__":
-    
+
     #Parse Arguments
     if len(sys.argv) < 3 or not os.path.isdir(os.path.abspath(sys.argv[1])) or not os.path.isdir(os.path.abspath(sys.argv[2])):
-        
+
         print('USAGE:\n \
               1st Argument: directory of controlPoints_*.csv\n \
               2nd Argument: directory of input images\n')
         sys.exit(1)
     csv_dir = os.path.abspath(sys.argv[1])
     png_dir = os.path.abspath(sys.argv[2])
-    
+
 
     if os.path.isfile(os.path.join(png_dir,'params_camchain.csv')):
         camchain_path = os.path.join(png_dir,'params_camchain.csv')
@@ -77,48 +80,48 @@ if __name__ == "__main__":
     else:
         print('ERROR: params_camchain.csv not found. Terminating...')
         sys.exit(1)
-        
+
     if os.path.isfile(os.path.join(png_dir,'params_R.csv')):
         R_path = os.path.join(png_dir,'params_camchain.csv')
         R = read_matrix_chain(R_path)
     else:
-        print('ERROR: params_camchain.csv not found. Terminating...')
+        print('ERROR: params_R.csv not found. Terminating...')
         sys.exit(1)
-    
-    
-    
+
+
     #Find all valid controlPoints.csv
-    csv_files = os.path.listdir(csv_dir)
+    csv_files = os.listdir(csv_dir)
     frame_indices = []
-    for i,file in csv_files:
+    for file in csv_files:
         # If we find a valid controlPoints*.csv, we extract the index and add it to the list of frames we want to validate.
         # Otherwise, we remove the file from the csv_files list.
         if file[:14] == 'controlPoints_' and file[-4:] == '.csv':
             frame_indices.append(int(file[14:-4]))
 
-            
+
     for frame in frame_indices:
         param_file = os.path.join(png_dir,"params_f_" + str(frame) + ".csv")
         if not os.path.isfile(param_file):
             print("Parameter .csv not found - skipping frame " + str(frame))
             continue
-        
+
         params = read_params(param_file)
-        
+
         for stream in range(N_STREAMS):
-            png_file = os.path.join(png_dir,"pic_s_" + str(stream) + "_f_" + str(frame) + ".png")
+            png_file = os.path.join(png_dir,"pic_s_" + str(stream+1) + "_f_" + str(frame) + ".png")
             if not os.path.isfile(png_file):
                 print("PNG for frame " + str(frame) + " stream " + str(stream) + " not found. Skipping.")
                 continue
-            
+
             projection_matrix = projection_matrix(camchain,R,stream)
-            cp4 = read_controlpoint(os.path.join(csv_dir,"controlPoints_" + str(frame) + ".csv")
-            cp2 = project_point(cp4,projection_matrix,params)
+            cp4 = read_controlpoint(os.path.join(csv_dir,"controlPoints_" + str(frame) + ".csv"))
+            cp2 = project_point(cp4,projection_matrix,params[stream])
+            cp2 = cp2.astype(np.int32)
             pic = cv2.imread(png_file)
 
             #Display control point in image
-            cv2.circle(pic,cp2,5,(255,255,0))
+            print(cp2)
+            cv2.circle(pic,(cp2[0],cp2[1]),5,(0,0,255),-1)
             cv2.imshow(os.path.basename(png_file),pic)
             cv2.waitKey(0)
-
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
