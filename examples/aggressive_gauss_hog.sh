b3008b23f3aa214db673497227a49fa4aae1a70b
Felice Serena <felice@serena-mueller.ch>
2018-05-22 14:34:09 +0200

    Bunch of examples added

--- examples/aggressive_gauss_hog.sh
+++ examples/aggressive_gauss_hog.sh
@@ -16,12 +16,24 @@ SRC=$1
 OUT=$2
 TRAIN=$3
 FRAMES="--first-frame=${4-0} --last-frame=${5-100000}"
-# CSV with HOG feature vectors
 
 $APP -c -s $SRC -l trace --pipeline-timer -o $OUT ${FRAMES} \
-	--pipeline-frame-window-filtering hog-labeling background-subtraction \
+	--pipeline-frame-window-filtering \
+		hog-labeling \
+		disparity-median \
+		disparity-morph-open\
+		disparity-morph-close\
+		disparity-morph-close\
+		disparity-morph-open\
+		disparity-gauss\
+		background-subtraction\
+		disparity-morph-open\
 	--hog-labeling-train=$TRAIN \
+	--disparity-gauss-k=10 \
+	--disparity-morph-open-diameter=10 \
+	--disparity-morph-close-diameter=10 \
+	--disparity-median-diameter=5 \
 	--background-subtraction-cage-directory=$SRC \
-	--pipeline-clustering=label-clustering
+	--pipeline-clustering=label-clustering 
 
 
